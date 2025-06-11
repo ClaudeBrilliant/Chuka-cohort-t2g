79fc2f1861060eba361952ff0f276030960b673e
Earl006 <earljoe06@gmail.com>
2025-06-11 07:41:14 +0300

    [Added]: cloudinary util to handle file uploads

--- hotel-management/src/app.module.ts
+++ hotel-management/src/app.module.ts
@@ -6,10 +6,11 @@ import { RoomsModule } from './rooms/rooms.module';
 import { RoomTypeModule } from './room_type/room_type.module';
 import { BookingModule } from './booking/booking.module';
 import { AuthModule } from './auth/auth.module';
+import { CloudinaryService } from './shared/utils/cloudinary/cloudinary.service';
 
 @Module({
   imports: [UsersModule, RoomsModule, RoomTypeModule, BookingModule, AuthModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, CloudinaryService],
 })
 export class AppModule {}
